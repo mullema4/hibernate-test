bbadd991cf71b8cae89098aec87014d3df982384
muller <muller@cngroup.dk>
2021-04-16 22:54:11 +0200

    Entity simplification

--- src/main/java/com/example/hibernatetest/Post.kt
+++ src/main/java/com/example/hibernatetest/Post.kt
@@ -1,14 +1,14 @@
 package com.example.hibernatetest
 
-import javax.persistence.*
+import javax.persistence.Entity
+import javax.persistence.FetchType.LAZY
+import javax.persistence.Id
+import javax.persistence.OneToOne
 
 @Entity
-class Post {
+class Post(
     @Id
-    @GeneratedValue
-    private val id: Long? = null
-
-    @OneToOne(cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
-    @JoinColumn(name = "post")
-    private val details: PostDetails? = null
-}
+    val id: Long? = null,
+    @OneToOne(fetch = LAZY)
+    val details: PostDetails? = null
+)
--- src/main/java/com/example/hibernatetest/PostDetails.kt
+++ src/main/java/com/example/hibernatetest/PostDetails.kt
@@ -1,15 +1,13 @@
 package com.example.hibernatetest
 
-import javax.persistence.CascadeType
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.OneToOne
 
 @Entity
-class PostDetails {
+class PostDetails(
     @Id
-    private val id: Long? = null
-
-    @OneToOne(mappedBy = "details", cascade = [CascadeType.ALL])
+    private val id: Long? = null,
+    @OneToOne(mappedBy = "details")
     private val post: Post? = null
-}
+)
