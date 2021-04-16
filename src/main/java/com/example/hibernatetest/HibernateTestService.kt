e132cfdcac5d88144b337e57237634f196b5e897
muller <muller@cngroup.dk>
2021-04-16 19:48:15 +0200

    SQL added

--- src/main/java/com/example/hibernatetest/HibernateTestService.kt
+++ src/main/java/com/example/hibernatetest/HibernateTestService.kt
@@ -12,4 +12,21 @@ class HibernateTestService(val repository: PostRepository) : ApplicationRunner {
     override fun run(args: ApplicationArguments) {
         repository!!.findAll()
     }
+
+    /*
+    Hibernate:
+    select post0_.id as id1_0_, post0_.post as post2_0_
+    from post post0_
+
+    Hibernate:
+    select postdetail0_.id as id1_1_0_, post1_.id as id1_0_1_, post1_.post as post2_0_1_
+    from post_details postdetail0_
+    left outer join post post1_ on postdetail0_.id=post1_.post
+    where postdetail0_.id=?
+
+    Hibernate:
+    select post0_.id as id1_0_0_, post0_.post as post2_0_0_
+    from post post0_
+    where post0_.post=?
+     */
 }
