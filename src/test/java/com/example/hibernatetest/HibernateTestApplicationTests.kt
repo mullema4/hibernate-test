e16ddf5831522c2273ec2bc1dc9216367ffbd460
muller <muller@cngroup.dk>
2021-04-16 19:49:57 +0200

    Test converted to Kotlin

--- src/test/java/com/example/hibernatetest/HibernateTestApplicationTests.kt
+++ src/test/java/com/example/hibernatetest/HibernateTestApplicationTests.kt
@@ -1,13 +1,11 @@
-package com.example.hibernatetest;
+package com.example.hibernatetest
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class HibernateTestApplicationTests {
-
+internal class HibernateTestApplicationTests {
     @Test
-    void contextLoads() {
+    fun contextLoads() {
     }
-
 }
